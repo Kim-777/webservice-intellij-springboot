a192ff55ba197d94cd5dea79e6725030bd7be399
kim-777 <iloo3012@naver.com>
2020-11-11 16:20:32 +0900

    게시글 삭제 기능 구현

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -8,6 +8,10 @@ let main = {
         $('#btn-update').on('click', function () {
             _this.update();
         });
+
+        $('#btn-delete').on('click', function () {
+            _this.delete();
+        });
     },
 
     save : function () {
@@ -48,6 +52,22 @@ let main = {
         }).done(function() {
             alert('글이 수정되었습니다.');
             window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+
+    delete : function () {
+        let id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/' +id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8'
+        }).done(function() {
+            alert('글이 삭제되었습니다.');
+            window.location.href = "/";
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
