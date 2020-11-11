64419a97e2fc7a23487faf0b5ef5fdba286d7ccd
kim-777 <iloo3012@naver.com>
2020-11-11 14:43:14 +0900

    전체 조회 화면 만들기

--- src/main/java/com/bbang/admin/service/posts/PostsService.java
+++ src/main/java/com/bbang/admin/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.bbang.admin.service.posts;
 
 import com.bbang.admin.domain.posts.Posts;
 import com.bbang.admin.domain.posts.PostsRepository;
+import com.bbang.admin.web.dto.PostsListResponseDto;
 import com.bbang.admin.web.dto.PostsResponseDto;
 import com.bbang.admin.web.dto.PostsSaveRequestDto;
 import com.bbang.admin.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +38,12 @@ public class PostsService {
                         IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
